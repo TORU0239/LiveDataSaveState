19c696d1508a4dd7b1d031d92c85de0625fdf799
Toru Choi <blueocean239@gmail.com>
2020-02-23 17:24:46 +0800

    add new viewmodel and dependencies

--- app/src/main/java/sg/toru/livedatasavestate/ui/main/activity/MainActivity.kt
+++ app/src/main/java/sg/toru/livedatasavestate/ui/main/activity/MainActivity.kt
@@ -3,15 +3,21 @@ package sg.toru.livedatasavestate.ui.main.activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.ViewGroup
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.SavedStateViewModelFactory
 import sg.toru.livedatasavestate.BR
 import sg.toru.livedatasavestate.R
 import sg.toru.livedatasavestate.databinding.ActivityMainBinding
+import sg.toru.livedatasavestate.ui.main.viewmodel.MainViewModel
 import sg.toru.livedatasavestate.ui.second.activity.SecondActivity
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
+    private val viewModel:MainViewModel by viewModels {
+        SavedStateViewModelFactory(application, this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val root = findViewById<ViewGroup>(R.id.container)
---
+++ app/src/main/java/sg/toru/livedatasavestate/ui/main/viewmodel/MainViewModel.kt
@@ -0,0 +1,8 @@
+package sg.toru.livedatasavestate.ui.main.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+// SaveState ViewModel
+class MainViewModel(private val saveStateHandle:SavedStateHandle):ViewModel() {
+}
